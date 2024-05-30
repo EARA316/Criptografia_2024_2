abf14d8e08a2b4e89c8b9b1f85d635f23160bbab
EARA316 <enriquealan.ra@gmail.com>
2024-05-30 14:45:23 -0600

    Archivos de ejemplos kyber y dilithium funcionales para casos

---
+++ Proyecto2/Programas Definitivos/ml-dsa/ml-dsa_dilithium2.py
@@ -0,0 +1,51 @@
+import oqs
+import time
+import resource
+
+# Obtención del uso de memoria antes de ejecutar el código
+before_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+message = "This is the message to sign".encode()
+
+print("Message to sign:", message)
+
+sigalg = "Dilithium2"
+
+# Create a signer instance using the specified algorithm
+signer = oqs.Signature(sigalg)
+
+# Key generation
+start_time = time.time()
+signer_public_key = signer.generate_keypair()
+keygen_time = time.time() - start_time
+
+# Signing
+start_time = time.time()
+signature = signer.sign(message)
+sign_time = time.time() - start_time
+
+# Verification
+verifier = oqs.Signature(sigalg)
+start_time = time.time()
+is_valid = verifier.verify(message, signature, signer_public_key)
+verify_time = time.time() - start_time
+
+# Obtención del uso de memoria después de ejecutar el código
+after_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+# Cálculo del uso de memoria
+memory_usage = after_memory - before_memory
+
+print("\nSignature algorithm:", sigalg)
+print("Signature details:")
+print("Public key length:", signer.details["length_public_key"])
+print("Secret key length:", signer.details["length_secret_key"])
+print("Signature length:", signer.details["length_signature"])
+print("Is EUF-CMA secure?", signer.details["is_euf_cma"])
+
+print("\nKey generation time:", keygen_time, "seconds")
+print("Signing time:", sign_time, "seconds")
+print("Verification time:", verify_time, "seconds")
+
+print("\nValid signature?", is_valid)
+print("Uso de memoria:", memory_usage, "bytes")
---
+++ Proyecto2/Programas Definitivos/ml-kem/ml-kem_kyber1024.py
@@ -0,0 +1,41 @@
+import oqs
+from pprint import pprint
+import time
+import resource
+
+# Obtención del uso de memoria antes de ejecutar el código
+before_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+kemalg = "Kyber1024"
+
+with oqs.KeyEncapsulation(kemalg) as client:
+    with oqs.KeyEncapsulation(kemalg) as server:
+        print("Key encapsulation details:")
+        pprint(client.details)
+
+        # Inicia el contador de tiempo para la generación de claves
+        start_time = time.time()
+        public_key_client = client.generate_keypair()
+        keygen_time = time.time() - start_time
+
+        # Inicia el contador de tiempo para la encapsulación
+        start_time = time.time()
+        ciphertext, shared_secret_server = server.encap_secret(public_key_client)
+        sign_time = time.time() - start_time
+
+        # Inicia el contador de tiempo para la desencapsulación
+        start_time = time.time()
+        shared_secret_client = client.decap_secret(ciphertext)
+        verify_time = time.time() - start_time
+
+# Obtención del uso de memoria después de ejecutar el código
+after_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+# Cálculo del uso de memoria
+memory_usage = after_memory - before_memory
+
+print("\nShared secretes coincide:", shared_secret_client == shared_secret_server)
+print(f"Key generation time: {keygen_time} seconds")
+print(f"Encapsulation time: {sign_time} seconds")
+print(f"Decapsulation time: {verify_time} seconds")
+print("Uso de memoria:", memory_usage, "bytes")
