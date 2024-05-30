491cba637501769d02c5c07a59285c4e917b9360
EARA316 <enriquealan.ra@gmail.com>
2024-05-30 13:37:01 -0600

    Archivo de ejemplo kyber mide memoria bro :0

--- Proyecto2/ml-kem_kyber512.py
+++ Proyecto2/ml-kem_kyber512.py
@@ -1,6 +1,10 @@
 import oqs
 from pprint import pprint
 import time
+import resource
+
+# Obtención del uso de memoria antes de ejecutar el código
+before_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
 kemalg = "Kyber512"
 
@@ -24,7 +28,14 @@ with oqs.KeyEncapsulation(kemalg) as client:
         shared_secret_client = client.decap_secret(ciphertext)
         verify_time = time.time() - start_time
 
+# Obtención del uso de memoria después de ejecutar el código
+after_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+# Cálculo del uso de memoria
+memory_usage = after_memory - before_memory
+
 print("\nShared secretes coincide:", shared_secret_client == shared_secret_server)
 print(f"Key generation time: {keygen_time} seconds")
 print(f"Encapsulation time: {sign_time} seconds")
 print(f"Decapsulation time: {verify_time} seconds")
+print("Uso de memoria:", memory_usage, "bytes")
